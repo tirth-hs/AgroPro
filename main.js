ddca5e6f652be8b6edb5f2343b4ed48f182f433a
tirth-hs <tirthshah200@gmail.com>
2020-04-24 22:05:50 +0530

    Completed news feed and removed unwanted files

--- main.js
+++ main.js
@@ -18,19 +18,35 @@ app.get('/crop.html',function(req,res){
     res.sendFile(path.join(__dirname+'/examples/crop.html'));
 })
 app.get('/news.html',function(req,res){
-    request('http://newsapi.org/v2/everything?q=rainfall in india&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
+    request('http://newsapi.org/v2/everything?qInTitle=rain&sources=the-hindu,the-times-of-india,google-news-in&sortBy=publishedAt&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
     var data = response.body
     var json = JSON.parse(data);
-    console.log(json.articles)
-    res.render('test',{data : json.articles})
+    var x,a,b,c,d
+    res.render('test',{data: json.articles.slice(10),x:"Rainfall",a:"success",b:"light",c:"light",d:"light" })
 });
 })
 app.get('/agriculture',function(req,res){
-    request('http://newsapi.org/v2/everything?q=agriculture in india&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
+    request('http://newsapi.org/v2/everything?qInTitle=agriculture OR crop&sources=the-hindu,the-times-of-india,google-news-in&sortBy=publishedAt&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
     var data = response.body
     var json = JSON.parse(data);
-    console.log(json.articles)
-    res.render('test',{data : json.articles})
+    var x,a,b,c,d
+    res.render('test',{data : json.articles.slice(10),x:"Agriculture",a:"light",b:"success",c:"light",d:"light"})
+});
+})
+app.get('/farmers',function(req,res){
+    request('http://newsapi.org/v2/everything?qInTitle=farmer OR kisan OR farmers  &sources=the-hindu,the-times-of-india,google-news-in&sortBy=publishedAt&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
+    var data = response.body
+    var json = JSON.parse(data);
+    var x,a,b,c,d
+    res.render('test',{data : json.articles.slice(10),x:"Farmers",a:"light",b:"light",c:"success",d:"light"})
+});
+})
+app.get('/irrigation',function(req,res){
+    request('http://newsapi.org/v2/everything?q=irrigation&sources=the-hindu,the-times-of-india,google-news-in&sortBy=publishedAt&apiKey=3b7021b74bfe474dac3f7a4786491e9b', function (error, response, body) {
+    var data = response.body
+    var json = JSON.parse(data);
+    var x,a,b,c,d,e
+    res.render('test',{data : json.articles.slice(10),x:"Irrigation",a:"light",b:"light",c:"light",d:"success"})
 });
 })
 app.get('/schemes.html',function(req,res){
